27aa1f990c8011d0f60dae8b594af842f9501b69
Benjamin. L. Whelan <whelbe@icloud.com>
2021-07-05 09:23:40 +0200

    Basic Styling for BlogPosts & Home
    - home styling
    - blogposts styling
    - prop information passing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="home">
+    <BlogPost :post='welcomeScreen' />  <!-- look for prop post in BlogPost component and equate to welcomeScreen property  -->
+    <BlogPost :post='post' v-for='(post, index) in sampleBlogPost' :key='index' />  <!-- for loop of array ~ index represents current object -->
   </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost';
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPost },
+  data() {  // populate firebase
+    return {
+      welcomeScreen: {
+        title: 'Welcome!',
+        blogPost:
+          "Weekly blog articles including everything that I am learning in programming since 2019.",
+        welcomeScreen: true,
+        photo: 'coding',
+      },
+      sampleBlogPost: [ // landing page posts
+        {
+          title: 'First blog post title!',
+          blogHTML: 'First blog post content!',
+          blogCoverPhoto: 'beautiful-stories',
+        },
+        {
+          title: 'Second blog post title!',
+          blogHTML: 'Second blog post content!',
+          blogCoverPhoto: 'designed-for-everyone',
+        },
+      ],
+    };
+  },
 };
 </script>
